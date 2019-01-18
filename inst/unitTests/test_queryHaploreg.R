81f4df00726d7d1b4ed845ea9d4f2e77f74c22b9
Ilya Y. Zhbannikov <ilya.zhbannikov@duke.edu>
2019-01-18 04:50:03 +0000

    version 2.0.7

--- inst/unitTests/test_queryHaploreg.R
+++ inst/unitTests/test_queryHaploreg.R
@@ -1,12 +1,19 @@
 test_queryHaploreg <- function() {
     # Test vector of SNPs
-    x <- queryHaploreg(query=c("rs10048158","rs4791078"))
+    x <- queryHaploreg(query=c("rs10048158","rs4791078"), 
+                       timeout=1000000)
     checkEqualsNumeric(dim(x)[1], 33)
     # Test loading from file
-    x <- queryHaploreg(file=system.file("extdata/snps.txt", package = "haploR"))
+    x <- queryHaploreg(file=system.file("extdata/snps.txt", package = "haploR"), 
+                       timeout=1000000)
     checkEqualsNumeric(dim(x)[1], 33)
     # Test loading from study
-    studies <- getStudyList()
-    x <- queryHaploreg(study=studies[[1]])
+    #studies <- getStudyList()
+    #x <- queryHaploreg(study=studies[[1]], 
+    #                   timeout=Haploreg.settings[["curl.timeout"]])
+    study <- list(name="Î²2-Glycoprotein I (Î²2-GPI) plasma levels (Athanasiadis G, 2013, 9 SNPs)",
+                 id="1756")
+    x <- queryHaploreg(study=study, 
+                       timeout=1000000)
     checkEqualsNumeric(dim(x)[1], 117)
 }
